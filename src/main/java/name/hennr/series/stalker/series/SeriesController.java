0c29bfb9f0f55e05b2acc6ecfb3724f6e03163d8
Jan-Hendrik Peters <hennr@hennr.name>
2018-02-13 22:33:16 +0100

    migrate to spring-boot 2.0RC1

--- src/main/java/name/hennr/series/stalker/series/SeriesController.java
+++ src/main/java/name/hennr/series/stalker/series/SeriesController.java
@@ -1,21 +1,19 @@
 package name.hennr.series.stalker.series;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.ModelAndView;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
 
-@RestController
+@Controller
 public class SeriesController {
 
     @Autowired
     private SeriesService seriesService;
 
-    @GetMapping("/series")
-    public ModelAndView listSeries() {
-        ModelAndView modelAndView = new ModelAndView("series");
-
-        modelAndView.addObject("seriesList", seriesService.getAllSeries());
-        return modelAndView;
+    @RequestMapping("/series")
+    public String listSeries(final Model model) {
+        model.addAttribute("seriesList", seriesService.getAllSeries());
+        return "series";
     }
 }
