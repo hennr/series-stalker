55c7618c487d4484ea7feb7ce9b5c24eedfb7c04
Jan-Hendrik Peters <jan-hendrik.peters@idealo.de>
2018-03-16 16:22:42 +0100

    add hystrix to fetch covers

--- src/main/java/name/hennr/series/stalker/tvmaze/SeriesClient.java
+++ src/main/java/name/hennr/series/stalker/tvmaze/SeriesClient.java
@@ -4,6 +4,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import com.netflix.hystrix.HystrixCommand;
+import com.netflix.hystrix.HystrixCommandGroupKey;
+
 @Service
 public class SeriesClient {
 
@@ -11,8 +14,28 @@ public class SeriesClient {
     private RestTemplate restTemplate;
 
     public String getImageUrlForSeriesWithId(String id) {
-        String url = "http://api.tvmaze.com/shows/" + id;
-        Series series = restTemplate.getForObject(url, Series.class);
-        return series.image.original;
+        return new HystrixSeriesClient(id).execute();
+    }
+
+    private class HystrixSeriesClient extends HystrixCommand<String> {
+
+        private String id;
+
+        HystrixSeriesClient(String id) {
+            super(HystrixCommandGroupKey.Factory.asKey("series"), 1500);
+            this.id = id;
+        }
+
+        @Override
+        protected String run() {
+            String url = "http://api.tvmaze.com/shows/" + id;
+            Series series = restTemplate.getForObject(url, Series.class);
+            return series.image.original;
+        }
+
+        @Override
+        protected String getFallback() {
+            return "";
+        }
     }
 }
