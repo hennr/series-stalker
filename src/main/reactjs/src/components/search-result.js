c958d5c18096f5274f5ae098e72e90b6d84b04ca
Jan-Hendrik Peters <jan-hendrik.peters@idealo.de>
2018-04-23 14:34:26 +0200

    show id in search results

--- src/main/reactjs/src/components/search-result.js
+++ src/main/reactjs/src/components/search-result.js
@@ -5,7 +5,7 @@ export default function SearchResult({searchResult}) {
     const searchItems = searchResult.map((searchItem) =>
         <div key={searchItem.id}>
             <img src={searchItem.image.medium} height="100" width="100"/>
-            {searchItem.name}
+            <p>{searchItem.name}: {searchItem.id}</p>
         </div>
     );
 
