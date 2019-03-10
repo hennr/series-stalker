f06d708458550452b6eb7450dfc2b3a44d5e4797
Jan-Hendrik Peters <hennr@hennr.name>
2019-03-10 15:17:12 +0100

    run flow during the build step

--- src/main/reactjs/src/components/TvMaze.js
+++ src/main/reactjs/src/components/TvMaze.js
@@ -1,8 +1,10 @@
+// @flow
+
 import TvMaze from "tv-maze";
 
 const tvMazeClient = TvMaze.createClient();
 
-export default function client(query, callback) {
+export default function client(query: string, callback: Function) {
     tvMazeClient.search(query, function (err, shows) {
         callback(shows);
     });
