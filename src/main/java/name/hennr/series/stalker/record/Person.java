036ca61de94825a98151b48311ee6eda97503bc2
Jan-Hendrik Peters <hennr@hennr.name>
2020-11-05 16:55:35 +0100

    bump spring-boot

--- src/main/java/name/hennr/series/stalker/record/Person.java
+++ src/main/java/name/hennr/series/stalker/record/Person.java
@@ -4,7 +4,6 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.validation.constraints.NotEmpty;
 
 @Entity
 public class Person {
@@ -12,7 +11,6 @@ public class Person {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
-    @NotEmpty
     private String data;
 
     public String getData() {
