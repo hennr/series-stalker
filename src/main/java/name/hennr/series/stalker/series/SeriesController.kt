2ab7a44c7e16d945c100089c423cea6bb7cd2f5b
Jan-Hendrik Peters <hennr@hennr.name>
2021-03-13 22:19:55 +0100

    kotlin autoconvert cleanup

--- src/main/java/name/hennr/series/stalker/series/SeriesController.kt
+++ src/main/java/name/hennr/series/stalker/series/SeriesController.kt
@@ -1,16 +1,15 @@
 package name.hennr.series.stalker.series
 
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 import reactor.core.publisher.Mono
 
 @RestController
-class SeriesController {
-    @Autowired
-    var seriesService: SeriesService? = null
+class SeriesController(val seriesService: SeriesService) {
+
     @GetMapping(value = ["/series/data"])
     fun seriesList(): Mono<List<SeriesModel>> {
-        return seriesService!!.allSeries
+        return seriesService.allSeries()
     }
+
 }
